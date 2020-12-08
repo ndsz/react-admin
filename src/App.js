04cc4431f3f61b779a0bd2afd1d0ed4ed8bc2fb5
niexianzhou.ty <niexianzhou.ty@devops.box>
2020-12-09 01:20:39 +0800

    d第二次修改

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Button } from 'antd'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './App.css'
 
-export default App;
+/**
+ * 应用的根组件
+ */
+
+ export default class App extends Component {
+   render () {
+     return (
+        <BrowserRouter>
+          <Switch>
+            <Route path="/login" component={Login}>Login</Route>
+            <Route path="/" component={Admin}>Admin</Route>
+          </Switch>
+        </BrowserRouter>
+     )
+   }
+ }
